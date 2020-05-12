64b7c608a3d85d3e471ea19ff21358339e867af1
Nimrod Kor <nimrodkor@gmail.com>
2020-05-12 14:42:45 +0300

    Add CheckResult and CheckCategories to exposed classes

--- bridgecrew/main.py
+++ bridgecrew/main.py
@@ -3,6 +3,8 @@
 from checkov.main import run as checkov_run
 # Add BaseResourceCheck to support extending checks
 from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
 from bridgecrew.banner import banner
 
 
