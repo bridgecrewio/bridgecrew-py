f0865237da61e073b2f594b77b4187b5de22cd08
Nimrod Kor <nimrodkor@gmail.com>
2020-05-12 14:35:55 +0300

    Support extending BaseResourceCheck through Bridgecrew

--- bridgecrew/main.py
+++ bridgecrew/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from checkov.main import run as checkov_run
-
+# Add BaseResourceCheck to support extending checks
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
 from bridgecrew.banner import banner
+
 
 def run():
     checkov_run(banner)
