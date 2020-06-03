544de55188d514d35cd23c8254aeb5f1cbaaf948
jonjozwiak <jonathan.jozwiak@googlemail.com>
2020-06-03 08:13:34 -0500

    Adding bridgecrew-k8s resources

---
+++ kubernetes/run_bridgecrew.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+################################################################################
+# Download all Kubernetes resources and scan with Bridgecrew Cloud CLI
+################################################################################
+
+# kubectl api-resources --verbs=list --namespaced -o name  | xargs -n1 -I{} bash -c "kubectl get {} --all-namespaces -oyaml && echo ---"
+RESOURCES="clusterroles
+clusterrolebindings
+configmaps
+cronjobs
+daemonsets
+deployments
+endpoints
+horizontalpodautoscalers
+ingresses
+jobs
+limitranges
+networkpolicies
+poddisruptionbudgets
+pods
+podsecuritypolicies
+replicasets
+replicationcontrollers
+resourcequotas
+roles
+rolebindings
+serviceaccounts
+services
+statefulsets"
+
+for resource in $RESOURCES;
+do
+  kubectl get $resource --all-namespaces -oyaml > /data/runtime.${resource}.yaml
+done
+
+if [ -f /etc/bridgecrew/apikey ]; then
+  apikey=$(cat /etc/bridgecrew/apikey)
+  if [ -f /etc/bridgecrew/repoid ]; then
+    repoid=$(cat /etc/bridgecrew/repoid)
+  else
+    repoid="runtime/unknown"
+  fi
+
+  bridgecrew -s -d /data --bc-api-key "$apikey" --repo-id "$repoid" --branch runtime
+else
+  bridgecrew -s -d /data
+fi
+
+
