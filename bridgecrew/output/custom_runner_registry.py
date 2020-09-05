33fe4cabc561e034d2fa82dd8c28e59c90664604
barak <schosterbarak@gmail.com>
2020-09-06 00:28:58 +0300

    support multi framework

--- bridgecrew/output/custom_runner_registry.py
+++ bridgecrew/output/custom_runner_registry.py
@@ -1,4 +1,5 @@
 import json
+from checkov.common.output.report import Report
 from checkov.common.runners.runner_registry import RunnerRegistry, OUTPUT_CHOICES
 
 from bridgecrew.output.sarif import SarifReport
@@ -14,20 +15,25 @@ class CustomRunnerRegistry(RunnerRegistry):
             print(f"{self.banner}\n")
         exit_codes = []
         report_jsons = []
-        for report in scan_reports:
-            if not report.is_empty():
-                if args.output == "json":
-                    report_jsons.append(report.get_dict())
-                elif args.output == "junitxml":
-                    report.print_junit_xml()
-                elif args.output == 'github_failed_only':
-                    report.print_failed_github_md()
-                elif args.output == 'sarif':
-                    report = self.report2sarif_report(report, args)
-                    report.print_sarif()
-                else:
-                    report.print_console(is_quiet=args.quiet)
-            exit_codes.append(report.get_exit_code(args.soft_fail))
+        merged_reports = self.merge_reports(scan_reports)
+        if args.output == 'sarif':
+            report = self.report2sarif_report(merged_reports, args)
+            report.print_sarif()
+        else:
+            for report in scan_reports:
+                if not report.is_empty():
+                    if args.output == "json":
+                        report_jsons.append(report.get_dict())
+                    elif args.output == "junitxml":
+                        report.print_junit_xml()
+                    elif args.output == 'github_failed_only':
+                        report.print_failed_github_md()
+                    elif args.output == 'sarif':
+                        report = self.report2sarif_report(report, args)
+                        report.print_sarif()
+                    else:
+                        report.print_console(is_quiet=args.quiet)
+                exit_codes.append(report.get_exit_code(args.soft_fail))
         if args.output == "json":
             if len(report_jsons) == 1:
                 print(json.dumps(report_jsons[0], indent=4))
@@ -35,6 +41,16 @@ class CustomRunnerRegistry(RunnerRegistry):
                 print(json.dumps(report_jsons, indent=4))
         exit_code = 1 if 1 in exit_codes else 0
         exit(exit_code)
+
+    @staticmethod
+    def merge_reports(reports):
+        merged_report = Report("merged")
+        for r in reports:
+            merged_report.skipped_checks.extend(r.skipped_checks)
+            merged_report.parsing_errors.extend(r.parsing_errors)
+            merged_report.passed_checks.extend(r.passed_checks)
+            merged_report.failed_checks.extend(r.failed_checks)
+        return merged_report
 
     @staticmethod
     def report2sarif_report(report, args):
