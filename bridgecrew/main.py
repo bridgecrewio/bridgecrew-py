d9e26f7cfbfdbf57557925ec51b45e9630951631
gruebel <anton.gruebel@gmail.com>
2023-03-13 11:38:22 +0100

    remove custom runner registry

--- bridgecrew/main.py
+++ bridgecrew/main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
-from checkov import main
-# Add BaseResourceCheck to support extending checks
-from bridgecrew.output.custom_runner_registry import BridgecrewRunnerRegistry
-from bridgecrew.banner import banner
-from checkov.main import DEFAULT_RUNNERS, Checkov
+from bridgecrew.banner import banner, tool
+from checkov.main import Checkov
 
 
 def run():
-    main.outer_registry = BridgecrewRunnerRegistry(banner, None, *DEFAULT_RUNNERS)
-    exit_code = Checkov().run(banner=banner)
+    exit_code = Checkov().run(banner=banner, tool=tool)
     if exit_code:
         exit(exit_code)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
